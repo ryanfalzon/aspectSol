12283fb6b4d54b6dda4dd9331780ccd367540f16
Ryan Falzon <musicfreakzryan@hotmail.com>
2022-02-08 22:40:21 +0100

    WIP - Added console application to run AspectSol compiler

---
+++ Source/AspectSol.App/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AspectSol.App;
+
+public class Program
+{
+    private readonly ILogger<Program> _logger;
+
+    public Program(ILogger<Program> logger)
+    {
+        _logger = logger;
+    }
+
+    public static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        host.Services.GetRequiredService<Program>().Run(args);
+    }
+
+    public void Run(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            _logger.LogError("Invalid arguments");
+        }
+
+        var command = args[0];
+
+        switch (command)
+        {
+            case "execute" when args.Length == 4:
+                {
+                    var aspectSolFilePath = args[1];
+                    var smartContractFilePath = args[2];
+                    var outputFilePath = args[3];
+
+
+                }
+                break;
+
+        }
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddTransient<Program>();
+            });
+    }
+}
---
+++ Source/AspectSol.Lib/App/AppService.cs
@@ -0,0 +1,29 @@
+﻿using AspectSol.Lib.Domain.Parsing;
+using AspectSol.Lib.Domain.Tokenization;
+using Microsoft.Extensions.Logging;
+
+namespace AspectSol.Lib.App;
+
+public class AppService
+{
+    private readonly ILogger<AppService> _logger;
+    private readonly ITokenizer _tokenizer;
+    private readonly IParser _parser;
+
+    public AppService(ILogger<AppService> logger, ITokenizer tokenizer, IParser parser)
+    {
+        _logger = logger;
+        _tokenizer = tokenizer;
+        _parser = parser;
+    }
+
+    public async Task Execute(string aspectSolFilePath, string smartContractFilePath, string outputFilePath)
+    {
+        var aspectSolFileContents = await File.ReadAllTextAsync(aspectSolFilePath);
+
+        var tokenizationResult = _tokenizer.Tokenize(aspectSolFileContents);
+        var parsingResult = _parser.Parse(tokenizationResult);
+
+        var smartContractFileContents = await File.ReadAllTextAsync(smartContractFilePath);
+    }
+}
